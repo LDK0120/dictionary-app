9d9bcca6b50dfaec274fe3edd40c175239ea99c2
Laura Kim <Doyeunkim120@gmail.com>
2022-03-01 00:34:13 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Link, Routes, Route} from "react-router-dom";
+import './App.css';
+import Home from './Home';
+import MyList from './MyList';
+
+export default function App() {
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/mylist">My List</Link>
+      </nav>
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/mylist" element={<MyList />} />
+      </Routes>
+    </div>
+  );
+}
+
