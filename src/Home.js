b53cd8182146d1791f9121a8150058785789b7c4
Laura Kim <Doyeunkim120@gmail.com>
2022-03-01 18:17:11 -0800

    pass searched word onto apiUrl

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,12 @@ import { Context } from "./Context.js"
 
 export default function Home() {
     const [state, setState] = useContext(Context) 
+    let wordToSearch = state.word
+    const apiKey = 'ddbc0656-4d77-4ac3-8e25-ab492ce02f2f'
+    const apiUrl = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${wordToSearch}?key=${apiKey}`
+
+    console.log(wordToSearch)
+    console.log(apiUrl)
     return (
         <div>
         <h1>Searched Word:</h1>
